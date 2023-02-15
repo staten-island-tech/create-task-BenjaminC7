4b2614286c12dbb974c6b097eb2237b804b4dd99
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-02-15 13:14:30 -0500

    I finished the first 5 seconds of  a game

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,30 +1,53 @@
 import "../styles/style.css";
 import { DOM } from "./DOM";
 
-async function startDeck() {
-  const response = await fetch(
-    `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
-  );
-  const deck = await response.json();
-  console.log(deck.success);
-  return deck.deck_id;
-}
-async function openingDraw(deckId) {
+async function openingDraw() {
   let playerValue = 0;
   const response = await fetch(
-    `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    `https://deckofcardsapi.com/api/deck/new/draw/?count=2`
   );
   const draws = await response.json();
   draws.cards.forEach((draw) => {
-    if ((draw.value = "1")) {
+    console.log(draw.value);
+    if (draw.value == "2") {
+      playerValue = playerValue + 2;
+    } else if (draw.value == "3") {
+      playerValue = playerValue + 3;
+    } else if (draw.value == "4") {
+      playerValue = playerValue + 4;
+    } else if (draw.value == "5") {
+      playerValue = playerValue + 5;
+    } else if (draw.value == "6") {
+      playerValue = playerValue + 6;
+    } else if (draw.value == "7") {
+      playerValue = playerValue + 7;
+    } else if (draw.value == "8") {
+      playerValue = playerValue + 8;
+    } else if (draw.value == "9") {
+      playerValue = playerValue + 9;
+    } else if (draw.value == "10") {
+      playerValue = playerValue + 10;
+    } else if (draw.value == "KING") {
+      playerValue = playerValue + 10;
+    } else if (draw.value == "JACK") {
+      playerValue = playerValue++ + 10;
+    } else if (draw.value == "QUEEN") {
+      playerValue = playerValue + 10;
+    } else if (draw.value == "ACE") {
+      if (playerValue <= 10) {
+        playerValue = playerValue + 11;
+      } else {
+        playerValue = playerValue + 1;
+      }
+    } else {
+      console.log("Draw Error.");
     }
   });
+  console.log(playerValue);
   return playerValue;
 }
 
 function startGame() {
-  startDeck().then((value) =>
-    openingDraw(value).then((value) => console.log(value))
-  );
+  openingDraw();
 }
 startGame();
