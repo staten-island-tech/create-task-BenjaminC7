8230235da4e2ebe008bec8e0bae1a2f1ede0cc1a
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-02-17 13:13:57 -0500

    hit works (mostly)

--- create-task/js/DOM.js
+++ create-task/js/DOM.js
@@ -2,5 +2,6 @@ const DOM = {
   startButton: document.getElementById("startButton"),
   hitButton: document.getElementById("hitButton"),
   standButton: document.getElementById("standButton"),
+  houseButton: document.getElementById("houseButton"),
 };
 export { DOM };
--- create-task/js/main.js
+++ create-task/js/main.js
@@ -55,8 +55,62 @@ async function startGame() {
   const deckId = await startDeck();
   let playerValue = await openingDraw(deckId);
   console.log(playerValue);
-  return playerValue;
+  let data = {
+    playerValue: playerValue,
+    deckId: deckId,
+  };
+  return data;
 }
-DOM.startButton.addEventListener("click", function () {
-  startGame();
+DOM.startButton.addEventListener("click", async function () {
+  let data = await startGame();
+  if (data.playerValue == 21) {
+    console.log("You Win");
+  }
+
+  DOM.hitButton.addEventListener("click", async function () {
+    let playerValue = data.playerValue;
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/${data.deckId}/draw/?count=1`
+    );
+    const draws = await response.json();
+    draws.cards.forEach((draw) => {
+      console.log(draw.value);
+      if (draw.value == "2") {
+        playerValue = playerValue + 2;
+      } else if (draw.value == "3") {
+        playerValue = playerValue + 3;
+      } else if (draw.value == "4") {
+        playerValue = playerValue + 4;
+      } else if (draw.value == "5") {
+        playerValue = playerValue + 5;
+      } else if (draw.value == "6") {
+        playerValue = playerValue + 6;
+      } else if (draw.value == "7") {
+        playerValue = playerValue + 7;
+      } else if (draw.value == "8") {
+        playerValue = playerValue + 8;
+      } else if (draw.value == "9") {
+        playerValue = playerValue + 9;
+      } else if (draw.value == "10") {
+        playerValue = playerValue + 10;
+      } else if (draw.value == "KING") {
+        playerValue = playerValue + 10;
+      } else if (draw.value == "JACK") {
+        playerValue = playerValue++ + 10;
+      } else if (draw.value == "QUEEN") {
+        playerValue = playerValue + 10;
+      } else if (draw.value == "ACE") {
+        if (playerValue <= 10) {
+          playerValue = playerValue + 11;
+        } else {
+          playerValue = playerValue + 1;
+        }
+      } else {
+        console.log("Draw Error.");
+      }
+    });
+    console.log(playerValue);
+    data.playerValue = playerValue;
+    return data;
+  });
 });
